57861b66edcb2deb6c55b0eba5d01b1148ed877b
Peter Wang <pw33537515@gmail.com>
2026-09-16 16:54:11 +1200

    update deduplication method on gpt

--- macros/insights.sql
+++ macros/insights.sql
@@ -5,8 +5,7 @@ raw_ads_insights AS (
   SAFE_CAST(JSON_VALUE(data,'$.impressions') AS INT64)  AS impressions,
   SAFE_CAST(JSON_VALUE(data,'$.spend') AS FLOAT64) AS media_cost,
   JSON_VALUE(data,'$.readable_time') AS date,
-  ROW_NUMBER() OVER (PARTITION BY JSON_VALUE(data,'$.id'),JSON_VALUE(data,'$.reasable_time'),
-  JSON_VALUE(data,'$.spend'),JSON_VALUE(data,'$.impressions'),JSON_VALUE(data,'$.clicks'),
+  ROW_NUMBER() OVER (PARTITION BY JSON_VALUE(data,'$.id'),
   JSON_VALUE(data,'$.readable_time')
   ORDER BY _sdc_batched_at DESC) AS row_num
 
